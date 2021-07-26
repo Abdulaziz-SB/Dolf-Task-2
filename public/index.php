9214372b71db1147cc033e139a2e10e54506ab6c
Abdulaziz-SB <azoz78861@gmail.com>
2021-07-26 15:50:56 +0300

    created sign-in page

--- public/index.php
+++ public/index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Cliparo</title>
 </head>
 <body>
-    <h1>ewer</h1>
+    <h1 class='text-primary-400 font-body text-6xl'>ewer</h1>
 </body>
 </html>
---
+++ src/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#F8EBE6',
+          200: '#FBC8BA',
+          300: '#FEDFA6',
+          400: '#B2CEC9',
+          500: '#EC894F',
+        }
+      },
+      fontFamily: {
+        body: ['Montserrat']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
